e94f4ca4522e7386ebf59b3e96aa29810f69c719
Cameron-Witcher <58893489+Cameron-Witcher@users.noreply.github.com>
2021-01-03 15:42:49 -0600

    dfs

--- src/main/java/net/mysticcloud/spigot/survival/commands/RandomTeleportCommand.java
+++ src/main/java/net/mysticcloud/spigot/survival/commands/RandomTeleportCommand.java
@@ -1,7 +1,5 @@
 package net.mysticcloud.spigot.survival.commands;
 
-import java.util.Random;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
@@ -16,8 +14,6 @@ import net.mysticcloud.spigot.survival.MysticVanilla;
 import net.mysticcloud.spigot.survival.utils.VanillaUtils;
 
 public class RandomTeleportCommand implements CommandExecutor {
-	private int circ = 360;
-	private int rad = 4000;
 
 	public RandomTeleportCommand(MysticVanilla plugin, String... cmd) {
 		for (String comd : cmd) {
@@ -31,8 +27,7 @@ public class RandomTeleportCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (sender instanceof Player) {
 			Location loc = ((Player) sender).getLocation().clone();
-			sender.sendMessage(CoreUtils.prefixes("rtp") + "Don't move. You will teleport in "
-					+ (sender.hasPermission("mysticcloud.teleport.waitoverride") ? 0 : 10) + " second(s).");
+			sender.sendMessage(CoreUtils.prefixes("rtp") + "Don't move. You will teleport in " + (sender.hasPermission("mysticcloud.teleport.waitoverride") ? 0 : 10) + " seconds.");
 			Bukkit.getScheduler().runTaskLater(VanillaUtils.getPlugin(), new Runnable() {
 
 				@Override
@@ -41,12 +36,11 @@ public class RandomTeleportCommand implements CommandExecutor {
 							&& ((Player) sender).getLocation().getBlockZ() == loc.getBlockZ()) {
 
 						Location loc2 = new Location(Bukkit.getWorld("survival"),
+								Math.cos(Math.toRadians(((CoreUtils.getRandom().nextInt(25132)) * (360 / 25132))))
+										* (CoreUtils.getRandom().nextInt(4000)),
+								0, Math.sin(Math.toRadians(((CoreUtils.getRandom().nextInt(25132)) * (360 / 25132)))));
 
-								Math.cos(Math.toRadians((new Random().nextInt(circ)) * (360 / circ) * (rad-10))), 0,
-								Math.sin(Math.toRadians((new Random().nextInt(circ)) * (360 / circ) * (rad-10))));
-
-						((Player) sender)
-								.teleport(loc2.getWorld().getHighestBlockAt(loc2).getLocation().add(0.5, 2, 0.5));
+						((Player) sender).teleport(loc2.getWorld().getHighestBlockAt(loc2).getLocation().add(0, 2, 0));
 						sender.sendMessage(CoreUtils.prefixes("rtp")
 								+ "You've been teleported to a random location in the survival world! Good luck! :)");
 					} else {
